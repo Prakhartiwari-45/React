891422deb74a7386fb5734640f34b26c7d2b39a5
Prakhartiwari-45 <prakhart240@gmail.com>
2026-05-26 13:31:22 +0530

    React Router Crash Course

---
+++ 07reactRouter/src/User/User.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+function User() {
+    const {id}=useParams()
+  return (
+    <div className='bg-green-600 text-3xl text-center'>User: {id}</div>
+  )
+}
+
+export default User
